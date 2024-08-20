11eee7212df1bf6f11d94dced442b0c1634c471b
Cleilson <137639044+cleilsonalvino@users.noreply.github.com>
2024-08-20 09:58:09 -0300

    agora vai

---
+++ dist/models/db.js
@@ -0,0 +1,49 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const sequelize_1 = require("sequelize");
+const dotenv = __importStar(require("dotenv"));
+dotenv.config();
+const sequelize = new sequelize_1.Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    logging: false,
+});
+const testarConexao = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Conectado com sucesso ao banco de dados!');
+    }
+    catch (erro) {
+        if (erro instanceof Error) {
+            console.error('Falha ao se conectar ao banco de dados:', erro.message);
+        }
+        else {
+            console.error('Falha ao se conectar ao banco de dados:', erro);
+        }
+    }
+};
+testarConexao();
+exports.default = sequelize;
