2b1a6754f78605b067f8e99ad828ce901848117d
Cleilson <137639044+cleilsonalvino@users.noreply.github.com>
2024-08-20 09:59:38 -0300

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -10,11 +10,11 @@ const port = 3000;
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../../public')));
 
 // Rota para renderizar a página principal
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public', 'index.html'));
+    res.sendFile(path.join(__dirname, '../../public', 'index.html'));
 });
 
 // Endpoint para obter todos os medicamentos
