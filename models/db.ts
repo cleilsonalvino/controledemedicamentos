25c4d9e4a105b968d8071d3532312f5cafe9e9ac
Cleilson <137639044+cleilsonalvino@users.noreply.github.com>
2024-08-20 00:00:58 -0300

    e

--- models/db.ts
+++ models/db.ts
@@ -1,7 +1,14 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize('sql10726424', 'sql10726424', 'L3iceNTDiF', {
-    host: 'sql10.freesqldatabase.com',
+
+// const sequelize = new Sequelize('sql10726424', 'sql10726424', 'L3iceNTDiF', {
+//     host: 'sql10.freesqldatabase.com',
+//     dialect: 'mysql',
+//     logging: false,
+// });
+
+const sequelize = new Sequelize(process.env.DB_NAME as string, process.env.DB_USER as string, process.env.DB_PASS as string, {
+    host: process.env.DB_HOST as string,
     dialect: 'mysql',
     logging: false,
 });
