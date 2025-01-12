53e4efb78836236122e13f9b36e9d85a2c2fb7fd
Cleilson <137639044+cleilsonalvino@users.noreply.github.com>
2025-01-12 08:02:54 -0300

    ok

--- models/db.ts
+++ models/db.ts
@@ -1,22 +1,22 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize('sql10726424', 'sql10726424', 'L3iceNTDiF', {
-    host: 'sql10.freesqldatabase.com',
-    dialect: 'mysql',
-    logging: false,
+const sequelize = new Sequelize("controledemedicamentos", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
+  logging: false,
 });
 
 const testarConexao = async () => {
-    try {
-        await sequelize.authenticate();
-        console.log('Conectado com sucesso ao banco de dados!');
-    } catch (erro) {
-        if (erro instanceof Error) {
-            console.error('Falha ao se conectar ao banco de dados:', erro.message);
-        } else {
-            console.error('Falha ao se conectar ao banco de dados:', erro);
-        }
+  try {
+    await sequelize.authenticate();
+    console.log("Conectado com sucesso ao banco de dados!");
+  } catch (erro) {
+    if (erro instanceof Error) {
+      console.error("Falha ao se conectar ao banco de dados:", erro.message);
+    } else {
+      console.error("Falha ao se conectar ao banco de dados:", erro);
     }
+  }
 };
 
 testarConexao();
